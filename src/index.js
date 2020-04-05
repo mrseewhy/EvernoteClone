0ac943eff10ce5f818d6e31551cc00514f203e6f
Olusegun Adeyemi <segunisaboy@gmail.com>
2020-04-05 07:27:07 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyB0Az0EWu09etJUvLMawKs6RTf8GZ68iFM",
+  authDomain: "evernote-82df7.firebaseapp.com",
+  databaseURL: "https://evernote-82df7.firebaseio.com",
+  projectId: "evernote-82df7",
+  storageBucket: "evernote-82df7.appspot.com",
+  messagingSenderId: "908062832242",
+  appId: "1:908062832242:web:57c1984f5ae711013674eb"
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
